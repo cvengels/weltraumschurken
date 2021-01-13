096fce836522b14d5887eb01ad174dc16c8dffb7
ctrlfreak453 <74403567+ctrlfreak453@users.noreply.github.com>
2021-01-13 10:55:39 +0100

    added testscene, player prefab and simple player movement within bounds
    +first scene
    +player prefab
    +playerController script
    public speed;minBound,maxBound

---
+++ Assets/scripts/playerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+
+    private Rigidbody2D player;
+    public float speed;
+    public float minBound, maxBound;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D> ();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > minBound && Input.GetKey(KeyCode.A))
+            player.velocity = new Vector2(-speed, 0);
+        else if (transform.position.x < maxBound && Input.GetKey(KeyCode.D))
+            player.velocity = new Vector2(speed, 0);
+        else
+            player.velocity = Vector2.zero;
+
+    }
+}
