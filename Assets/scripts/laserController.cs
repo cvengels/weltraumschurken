e6f9a7363148fa4616e3960d5387d937aa8512fd
ctrlfreak453 <74403567+ctrlfreak453@users.noreply.github.com>
2021-01-13 12:29:20 +0100

    added laser weapons fire
    +laser prefab
    +laserController script
     playerController
    +public gameobject laser
    +public firerate

---
+++ Assets/scripts/laserController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laserController : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D laser;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        laser = GetComponent<Rigidbody2D>();
+        laser.velocity = new Vector2(0, speed);
+
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -9,6 +9,9 @@ public class playerController : MonoBehaviour
     private Rigidbody2D player;
     public float speed;
     public float minBound, maxBound;
+    public GameObject laser;
+    public float firerate;
+    private float nextfire;
     
     // Start is called before the first frame update
     void Start()
@@ -20,12 +23,23 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (transform.position.x > minBound && Input.GetKey(KeyCode.A))
             player.velocity = new Vector2(-speed, 0);
         else if (transform.position.x < maxBound && Input.GetKey(KeyCode.D))
             player.velocity = new Vector2(speed, 0);
         else
             player.velocity = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.Space) && Time.time > nextfire)
+        {
+            nextfire = Time.time + firerate;
+            float x = transform.position.x;
+            float y = transform.position.y;
+            Instantiate(laser, new Vector2(x, y), Quaternion.identity);
+
+        }
+
 
     }
 }
